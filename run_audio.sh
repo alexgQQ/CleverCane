807be364b78db2e008d2fa8cd2d607fb6a3420b6
alexQQ <grand@ad.cu.edu>
2017-12-11 10:28:26 -0700

    deleting old files before adding updated project

--- run_audio.sh
+++
@@ -1,3 +0,0 @@
-amixer -c 0 cset iface=MIXER,name='RX3 MIX1 INP1' 'RX1'
-amixer -c 0 cset iface=MIXER,name='SPK DAC Switch' 1
-aplay -c 1 -D plughw:0,1 piano2.wav
--- utils/app_utils.py
+++ utils/app_utils.py
@@ -46,8 +46,8 @@ class WebcamVideoStream:
         # initialize the video camera stream and read the first frame
         # from the stream
         self.stream = cv2.VideoCapture(src)
-        self.stream.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-        self.stream.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+        self.stream.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, width)
+        self.stream.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, height)
         (self.grabbed, self.frame) = self.stream.read()
 
         # initialize the variable used to indicate if the thread should
